964edebd52aa6093c0a3eeb0e8e458c9a0e77005
al3amri <49554966+al3amri@users.noreply.github.com>
2023-03-08 04:17:28 +0400

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,22 +1,29 @@
-
-
 function download_image() {
-    var url = "https://instagram.com/" + document.getElementById('username').value + "?__a=1";
+    var username = document.getElementById('username').value;
+    var url = "https://instagram.com/" + username + "?__a=1";
 
     $("#download").html('Loading');
 
     $.ajax({
-        url: url,
+        url: "https://api.allorigins.win/get?url=" + encodeURIComponent(url) + "&callback=?",
         type: 'get',
+        dataType: 'jsonp',
         success: function (response) {
-            var profile = response.graphql.user.profile_pic_url_hd;
+            var data = JSON.parse(response.contents);
+            if (data.graphql && data.graphql.user) {
+                var profile = data.graphql.user.profile_pic_url_hd;
 
-            $("#image").attr("src", profile);
-            document.getElementById("image").style.display = "block";
-            $("#download").html('View');
+                $("#image").attr("src", profile);
+                document.getElementById("image").style.display = "block";
+                $("#download").html('View');
+            } else {
+                alert("User Not Found");
+                $("#download").html('View');
+                document.getElementById("image").style.display = "none";
+            }
         },
         error: function (error) {
-            alert("User Not Found");
+            alert("Error fetching data");
             $("#download").html('View');
             document.getElementById("image").style.display = "none";
         }
@@ -24,4 +31,3 @@ function download_image() {
     });
 
 }
-
